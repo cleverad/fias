542e6cd52389cea117117870985fae57516e4836
marvin255 <postbox433@gmail.com>
2018-09-26 10:12:12 +0500

    add comment to base test class

--- tests/src/BaseTestCase.php
+++ tests/src/BaseTestCase.php
@@ -5,6 +5,12 @@ namespace marvin255\fias\tests;
 use PHPUnit\Framework\TestCase;
 use Faker;
 
+/**
+ * Базовый класс для всех тестов, все тесты в библиотеке должны наследоваться
+ * от него.
+ *
+ * Реализует дополнительный функционал, например, обращение к faker.
+ */
 abstract class BaseTestCase extends TestCase
 {
     /**
