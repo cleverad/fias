5d37fbdcfc1659f116b66f375e5352dc3ccaa9d7
marvin255 <postbox433@gmail.com>
2018-10-15 14:44:11 +0500

    add DbInterface

---
+++ src/service/db/DbInterface.php
@@ -0,0 +1,90 @@
+<?php
+
+declare(strict_types=1);
+
+namespace marvin255\fias\service\db;
+
+use marvin255\fias\mapper\SqlMapperInterface;
+
+/**
+ * Интерфейс для объекта, который реализует взаимодействие с базой данных.
+ */
+interface DbInterface
+{
+    /**
+     * Добавляет новую строку в базу данных.
+     *
+     * @param \marvin255\fias\mapper\SqlMapperInterface $mapper
+     * @param array                                     $item
+     *
+     * @return void
+     *
+     * @throws \marvin255\fias\service\db\Exception
+     */
+    public function insert(SqlMapperInterface $mapper, array $item);
+
+    /**
+     * Обновляет строку в базе данных.
+     *
+     * @param \marvin255\fias\mapper\SqlMapperInterface $mapper
+     * @param array                                     $item
+     *
+     * @return void
+     *
+     * @throws \marvin255\fias\service\db\Exception
+     */
+    public function update(SqlMapperInterface $mapper, array $item);
+
+    /**
+     * Удаляет строку из базы данных.
+     *
+     * @param \marvin255\fias\mapper\SqlMapperInterface $mapper
+     * @param array                                     $item
+     *
+     * @return void
+     *
+     * @throws \marvin255\fias\service\db\Exception
+     */
+    public function delete(SqlMapperInterface $mapper, array $item);
+
+    /**
+     * Создает таблицу в базе данных, согласно описанию из маппера.
+     *
+     * @param \marvin255\fias\mapper\SqlMapperInterface $mapper
+     *
+     * @return void
+     *
+     * @throws \marvin255\fias\service\db\Exception
+     */
+    public function createTable(SqlMapperInterface $mapper);
+
+    /**
+     * Удалает таблицу в базе данных, согласно описанию из маппера.
+     *
+     * @param \marvin255\fias\mapper\SqlMapperInterface $mapper
+     *
+     * @return void
+     *
+     * @throws \marvin255\fias\service\db\Exception
+     */
+    public function deleteTable(SqlMapperInterface $mapper);
+
+    /**
+     * Очищает от содержимого таблицу в базе данных, согласно описанию из маппера.
+     *
+     * @param \marvin255\fias\mapper\SqlMapperInterface $mapper
+     *
+     * @return void
+     *
+     * @throws \marvin255\fias\service\db\Exception
+     */
+    public function truncateTable(SqlMapperInterface $mapper);
+
+    /**
+     * Указывает, что данный потребитель закончил работу с базой данных и нужно
+     * дописать все оставшиеся запросы и сбросить все временные данные.
+     *
+     * @return void
+     */
+    public function complete();
+}
---
+++ src/service/db/Exception.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace marvin255\fias\service\db;
+
+/**
+ * Исключение, которое будет выброшено для ошибок в работе с базой данных.
+ */
+class Exception extends \Exception
+{
+}
