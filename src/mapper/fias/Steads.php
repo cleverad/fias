3d3d03dab5ebf1a9b0fbb9d55ac7bee9fe1fd537
marvin255 <postbox433@gmail.com>
2018-11-13 11:59:20 +0500

    add Steads mapper

---
+++ src/mapper/fias/Steads.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace marvin255\fias\mapper\fias;
+
+use marvin255\fias\mapper\AbstractMapper;
+use marvin255\fias\mapper\field\Line;
+use marvin255\fias\mapper\field\Date;
+
+/**
+ * Земельные участки.
+ */
+class Steads extends AbstractMapper
+{
+    /**
+     * @inheritdoc
+     */
+    protected function createFields(): array
+    {
+        return [
+            'STEADGUID' => new Line(36),
+            'NUMBER' => new Line,
+            'REGIONCODE' => new Line(2),
+            'POSTALCODE' => new Line(6),
+            'IFNSFL' => new Line(4),
+            'IFNSUL' => new Line(4),
+            'OKATO' => new Line(11),
+            'OKTMO' => new Line(11),
+            'PARENTGUID' => new Line(36),
+            'STEADID' => new Line(36),
+            'OPERSTATUS' => new Line,
+            'STARTDATE' => new Date,
+            'ENDDATE' => new Date,
+            'UPDATEDATE' => new Date,
+            'LIVESTATUS' => new Line,
+            'DIVTYPE' => new Line,
+            'NORMDOC' => new Line(36),
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getXmlPath(): string
+    {
+        return '/Steads/Stead';
+    }
+
+    /**
+     * @inhertitdoc
+     */
+    public function getInsertFileMask(): string
+    {
+        return 'AS_STEAD_*.XML';
+    }
+
+    /**
+     * @inhertitdoc
+     */
+    public function getDeleteFileMask(): string
+    {
+        return 'AS_DEL_STEAD_*.XML';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getSqlPrimary(): array
+    {
+        return ['STEADGUID'];
+    }
+}
---
+++ tests/src/mapper/fias/SteadsTest.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace marvin255\fias\tests\mapper\fias;
+
+use marvin255\fias\mapper\fias\Steads;
+use marvin255\fias\mapper\MapperInterface;
+use DateTime;
+
+/**
+ * Тест маппера Steads.
+ */
+class SteadsTest extends MapperCase
+{
+    /**
+     * Возвращает данные для проверки извлечения из xml.
+     */
+    protected function getXmlTestData(): array
+    {
+        $data = [
+            'STEADGUID' => $this->faker()->uuid,
+            'NUMBER' => $this->faker()->word,
+            'REGIONCODE' => $this->faker()->word,
+            'POSTALCODE' => $this->faker()->word,
+            'IFNSFL' => $this->faker()->word,
+            'IFNSUL' => $this->faker()->word,
+            'OKATO' => $this->faker()->word,
+            'OKTMO' => $this->faker()->word,
+            'PARENTGUID' => $this->faker()->uuid,
+            'STEADID' => $this->faker()->uuid,
+            'OPERSTATUS' => $this->faker()->word,
+            'STARTDATE' => new DateTime($this->faker()->date),
+            'ENDDATE' => new DateTime($this->faker()->date),
+            'UPDATEDATE' => new DateTime($this->faker()->date),
+            'LIVESTATUS' => $this->faker()->word,
+            'DIVTYPE' => $this->faker()->word,
+            'NORMDOC' => $this->faker()->uuid,
+        ];
+
+        $xml = '<Stead';
+        $xml .= " STEADGUID=\"{$data['STEADGUID']}\"";
+        $xml .= " NUMBER=\"{$data['NUMBER']}\"";
+        $xml .= " REGIONCODE=\"{$data['REGIONCODE']}\"";
+        $xml .= " POSTALCODE=\"{$data['POSTALCODE']}\"";
+        $xml .= " IFNSFL=\"{$data['IFNSFL']}\"";
+        $xml .= " IFNSUL=\"{$data['IFNSUL']}\"";
+        $xml .= " OKATO=\"{$data['OKATO']}\"";
+        $xml .= " OKTMO=\"{$data['OKTMO']}\"";
+        $xml .= " PARENTGUID=\"{$data['PARENTGUID']}\"";
+        $xml .= " STEADID=\"{$data['STEADID']}\"";
+        $xml .= " OPERSTATUS=\"{$data['OPERSTATUS']}\"";
+        $xml .= ' STARTDATE="' . $data['STARTDATE']->format('d.m.Y') . '"';
+        $xml .= ' ENDDATE="' . $data['ENDDATE']->format('d.m.Y') . '"';
+        $xml .= ' UPDATEDATE="' . $data['UPDATEDATE']->format('d.m.Y') . '"';
+        $xml .= " LIVESTATUS=\"{$data['LIVESTATUS']}\"";
+        $xml .= " DIVTYPE=\"{$data['DIVTYPE']}\"";
+        $xml .= " NORMDOC=\"{$data['NORMDOC']}\"";
+        $xml .= ' NEVER_GET_ME="NEVER_GET_ME"';
+        $xml .= ' />';
+
+        return [$data, $xml];
+    }
+
+    /**
+     * Возвращает объект маппера.
+     */
+    protected function getMapper(): MapperInterface
+    {
+        return new Steads;
+    }
+}
