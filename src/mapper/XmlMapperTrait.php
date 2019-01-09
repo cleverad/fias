d2bf3c377efedf779da782be6e42c20aeb6d39b7
marvin255 <postbox433@gmail.com>
2019-01-09 12:15:36 +0500

    fix

--- src/mapper/XmlMapperTrait.php
+++ src/mapper/XmlMapperTrait.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace marvin255\fias\mapper;
 
 use SimpleXMLElement;
+use LibXMLError;
 use Throwable;
 use RuntimeException;
 
@@ -125,6 +126,9 @@ trait XmlMapperTrait
         $errors = [];
 
         foreach (libxml_get_errors() as $error) {
+            if ($error instanceof LibXMLError) {
+                $errors[] = $error->message;
+            }
         }
 
         libxml_clear_errors();
