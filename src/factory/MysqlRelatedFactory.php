e4174ce63c9bacf60aa3b066fd68000e6381fe0b
marvin255 <postbox433@gmail.com>
2019-01-15 17:28:36 +0500

    add abstract command for cli

--- src/factory/MysqlRelatedFactory.php
+++ src/factory/MysqlRelatedFactory.php
@@ -10,6 +10,10 @@ use marvin255\fias\task\DownloadFull;
 use marvin255\fias\task\Unpack;
 use marvin255\fias\task\CreateStructure;
 use marvin255\fias\task\MysqlLoadDataInsert;
+use PDO;
+use Exception;
+use InvalidArgumentException;
+use RuntimeException;
 
 /**
  * Фабричный объект, который создает пайпы для соответствующих типов задач,
@@ -52,5 +56,32 @@ class MysqlRelatedFactory extends InternalServicesFactory
         $pipe->setCleanup(new Cleanup($log));
 
         return $pipe;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function createPdo(): PDO
+    {
+        $dsn = $this->config->getString('pdo_dsn', '');
+        $user = $this->config->getString('pdo_user', '');
+        $password = $this->config->getString('pdo_password', '');
+
+        if (!$dsn) {
+            throw new InvalidArgumentException('Empty pdo_dsn config parameter');
+        }
+        if (!$user) {
+            throw new InvalidArgumentException('Empty pdo_user config parameter');
+        }
+
+        try {
+            $pdo = new PDO($dsn, $user, $password, [
+                PDO::MYSQL_ATTR_LOCAL_INFILE => true,
+            ]);
+        } catch (Exception $e) {
+            throw new RuntimeException($e->getMessage(), (int) $e->getCode(), $e);
+        }
+
+        return $pdo;
     }
 }
